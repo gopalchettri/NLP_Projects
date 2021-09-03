1c9438399a0404d00191a2bfa036631627792a66
Gopal Chettri <gopalchettri@gmail.com>
2021-09-03 16:47:34 +0530

    Resume parser

---
+++ resume_parser/setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages
+from setuptools import setup
+
+
+setup(name='keras_en_parser_and_analyzer',
+      version='0.0.1',
+      description='English Parser and Analyzer using recurrent network in Keras',
+      author='',
+      author_email='',
+      url='',
+      download_url='',
+      license='',
+      install_requires=['Keras'],
+      packages=find_packages())
